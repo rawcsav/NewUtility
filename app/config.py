0ab4296e9ad1bd1b118fe66cb972aa35b81409c1
rawc <gmasontf2@gmail.com>
2023-09-20 10:47:37 -0400

    tokens/top_n change, docs scroll bar

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ import tiktoken
 SECRET_KEY = os.getenv("SECRET_KEY")
 SESSION_TYPE = "filesystem"
 SESSION_PERMANENT = True
-PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+PERMANENT_SESSION_LIFETIME = timedelta(minutes=180)
 
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
@@ -21,8 +21,8 @@ TOKENIZER = tiktoken.get_encoding("cl100k_base")
 EMBEDDING_MODEL = "text-embedding-ada-002"
 GPT_MODEL = "gpt-4"
 MAIN_TEMP_DIR = "app/main_user_directory"
-MAX_LENGTH = 300
-TOP_N = 4
+MAX_LENGTH = 250
+TOP_N = 6
 BATCH_SIZE = 10
 ALLOWED_EXTENSIONS = {"txt", "pdf", "docx"}
 
