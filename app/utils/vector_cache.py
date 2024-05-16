dd14cacc55c546e5d1f80dc5769f79ef5d11176b
rawc <gmasontf2@gmail.com>
2024-05-16 00:00:16 -0400

    embeddings error handling

--- app/utils/vector_cache.py
+++ app/utils/vector_cache.py
@@ -1,10 +1,11 @@
 import pickle
 import threading
-
 import numpy as np
 from sqlalchemy.orm import load_only
 
 from app.models.embedding_models import Document, DocumentChunk, DocumentEmbedding
+
+
 class VectorCache:
     _instance = None
     _vectors = np.array([])  # Store vectors as a single numpy array
@@ -35,11 +36,13 @@ class VectorCache:
         )
 
         if not embeddings:
+            cls.clear_cache()
             return
 
         with cls._lock:
             cls._vectors = np.stack([np.frombuffer(embedding.embedding, dtype=np.float32) for embedding in embeddings])
             cls._ids = [str(embedding.chunk_id) for embedding in embeddings]
+
     @classmethod
     def mips_naive(cls, query_vector: np.ndarray, subset_ids: list) -> list:
         if not isinstance(query_vector, np.ndarray):
@@ -48,6 +51,9 @@ class VectorCache:
             raise ValueError("Query vector must be a 1D array.")
 
         with cls._lock:
+            if cls._vectors.size == 0:
+                return []
+
             subset_indices = [cls._ids.index(str(chunk_id)) for chunk_id in subset_ids if str(chunk_id) in cls._ids]
             subset_vectors = cls._vectors[subset_indices]
 
@@ -55,6 +61,7 @@ class VectorCache:
         similarities = np.dot(subset_vectors, query_vector)
 
         # Combine IDs and similarities, then sort by similarity score in descending order
-        ranked_similarities = sorted(zip([cls._ids[i] for i in subset_indices], similarities), key=lambda x: x[1], reverse=True)
+        ranked_similarities = sorted(zip([cls._ids[i] for i in subset_indices], similarities), key=lambda x: x[1],
+                                     reverse=True)
 
         return [(chunk_id, rank + 1) for rank, (chunk_id, _) in enumerate(ranked_similarities)]
